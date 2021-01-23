572fe2dec390e25fa442831586e3c150b011ebbf
Buğra Arslan <bgra.arsln@hotmail.com>
2021-01-23 23:55:31 +0300

    23.01.2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager, new DatabaseLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { };
+
+            krediler.Add(konutKrediManager);
+            krediler.Add(tasitKrediManager);
+            krediler.Add(ihtiyacKrediManager);
+            krediler.Add(esnafKrediManager);
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
